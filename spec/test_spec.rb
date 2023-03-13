fdec501333637d66461a710cf28853dd35e97c05
James Couball <jcouball@yahoo.com>
2023-03-13 08:50:42 -0700

    Improve spec by making the WrappedIO class dynamic
    Signed-off-by: James Couball <jcouball@yahoo.com>

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -1,33 +1,35 @@
 require 'English'
 require 'tmpdir'
 
-class WrappedIO
-  def initialize(io_writer)
-    @io_writer = io_writer
-    @to_io_called = false
-  end
-
-  def to_io
-    @to_io_called = true
-    @io_writer
-  end
-
-  def to_io_called?
-    @to_io_called
-  end
-end
-
 describe 'Process#spawn' do
+  let(:wrapped_io_class) do
+    Class.new do
+      def initialize(io_writer)
+        @io_writer = io_writer
+        @to_io_called = false
+      end
+
+      def to_io
+        @to_io_called = true
+        @io_writer
+      end
+
+      def to_io_called?
+        @to_io_called
+      end
+    end
+  end
+
   context 'when called with out: set to an instance of WrappedIO' do
     it 'should not raise an error' do
-      out = WrappedIO.new(STDOUT)
+      out = wrapped_io_class.new(STDOUT)
       expect do
         Process.wait(Process.spawn('exit 0', out: out))
       end.not_to raise_error
     end
 
     it 'should call #to_io to get the wrapped IO object' do
-      out = WrappedIO.new(STDOUT)
+      out = wrapped_io_class.new(STDOUT)
       Process.wait(Process.spawn('exit 0', out: out))
       expect(out.to_io_called?).to eq(true)
     end
@@ -36,7 +38,7 @@ describe 'Process#spawn' do
       Dir.mktmpdir do |dir|
         Dir.chdir(dir) do
           f = File.open('out.txt', 'wb')
-          out = WrappedIO.new(f)
+          out = wrapped_io_class.new(f)
           Process.wait(Process.spawn('echo "Hello World"', out: out))
           f.close
 
